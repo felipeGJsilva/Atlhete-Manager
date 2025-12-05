c100af8f5393016cfbaa968f1e1c2e87ef5c69c7
felipeGJsilva <felipejansen4@gmail.com>
2025-12-05 19:49:46 -0300

    feito

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
+app = Flask(
+    __name__,
+    template_folder="views",      # <-- muda aqui
+    static_folder="static"
+)
 
 @app.route("/")
 def home():
-    return render_template("base/base.html")
+    return render_template("base/base.html")  # funciona normalmente
 
 if __name__ == "__main__":
     app.run(debug=True)
